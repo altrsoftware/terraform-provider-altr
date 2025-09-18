4dfcca5a45b77221b49a391971d6db9deccce366
Lee Deffebach <lee@altr.com>
2025-09-18 14:17:39 -0600

    update examples

--- examples/data-sources/altr_access_management_oltp_policy/data-source.tf
+++ examples/data-sources/altr_access_management_oltp_policy/data-source.tf
@@ -2,5 +2,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 data "altr_access_management_oltp_policy" "example" {
-  id = "example"
+  id = "OLTP#20b8b5df-3b72-4a24-9dc5-cf854ba81f07#POLICY"
 }
--- examples/data-sources/altr_impersonation_policy/data-source.tf
+++ examples/data-sources/altr_impersonation_policy/data-source.tf
@@ -2,6 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 data "altr_access_management_oltp_policy" "example" {
-  id = "example"
+  id = "IMPERSONATION#20b8b5df-3b72-4a24-9dc5-cf854ba81f07#POLICY"
 }
 
